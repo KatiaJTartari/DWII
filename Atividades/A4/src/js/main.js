994ea8e154d5ae38cd0f9963d59c28854a1115ee
KatiaJTartari <70042571+KatiaJTartari@users.noreply.github.com>
2021-11-03 23:09:01 -0300

    Add files via upload

--- Atividades/A4/src/js/main.js
+++ Atividades/A4/src/js/main.js
@@ -1,30 +1,119 @@
-//main.js
-
 import { masks } from './modules/mask.js';
 import { valids } from './modules/valid.js';
 
+//Nome
+document.querySelectorAll('input.mask').forEach($input => {
+    const field = $input.dataset.js
+    $input.addEventListener(
+        'input',
+        e => {
+            if (field != 'nome')
+        e.target.value = masks[field](e.target.value)
+        }, 
+    false
+    )
+})
+// ######### Validação nome ##########
+// Busca o campo nome e escuta o evento de entrada de texto
+document.querySelector('#nome').addEventListener('input', e => {
+    // Atribui(Guarda) a true ou false dependendo se o nome é válido
+    let is_valid = valids.nome(e.target.value)
+  
+    //Adiciona a classe que deixa a borada vermelha, indicando que o nome é inválido
+    if (!is_valid) e.target.classList.add('errorInput')
+    //REtira a classe que deixa a borada vermelha, indicando que o nome é válido
+    else e.target.classList.remove('errorInput')
+  })
+  
+//cpf
 document.querySelectorAll('input.mask').forEach($input => {
     const field = $input.dataset.js
     //console.log(field) 
     $input.addEventListener(
         'input',
         e => {
+            if (field != 'cpf')
         e.target.value = masks[field](e.target.value)
        // e.target.value = masks.cpf(e.target.value)
     }, 
     false
     )
-})
+})  
+//Validação CPF
+document.querySelector('#cpf').addEventListener('input', e => {
+    let is_valid = valids.cpf(e.target.value)
+    if (!is_valid) e.target.classList.add('errorInput')
+    else e.target.classList.remove('errorInput')
+  })
 
-document.querySelectorAll('input.valid').forEach($input => {
+//data de nascimento
+document.querySelectorAll('input.mask').forEach($input => {
     const field = $input.dataset.js
     $input.addEventListener(
         'input',
         e => {
-        e.target.value = valids[field](e.target.value)
-        //e.target.value = valids.cpf(e.target.value)
+            if (field != 'dt_nasc')
+        e.target.value = masks[field](e.target.value)
         }, 
     false
     )
 })
+document.querySelector('#dt_nasc').addEventListener('input', e => {
+    let is_valid = valids.dt_nasc(e.target.value)
+    if (!is_valid) e.target.classList.add('errorInput')
+    else e.target.classList.remove('errorInput')
+})    
+        
+//email
+document.querySelectorAll('input.mask').forEach($input => {
+    const field = $input.dataset.js
+    $input.addEventListener(
+        'input',
+        e => {
+            if (field != 'email')
+        e.target.value = masks[field](e.target.value)
+        }, 
+    false
+    )
+})
+document.querySelector('#email').addEventListener('input', e => {
+    let is_valid = valids.email(e.target.value)
+    if (!is_valid) e.target.classList.add('errorInput')
+    else e.target.classList.remove('errorInput')
+})            
+       
+//telefone
+document.querySelectorAll('input.mask').forEach($input => {
+    const field = $input.dataset.js
+    $input.addEventListener(
+        'input',
+        e => {
+            if (field != 'fone')
+        e.target.value = masks[field](e.target.value)
+        }, 
+    false
+    )
+})
+document.querySelector('#fone').addEventListener('input', e => {
+    let is_valid = valids.fone(e.target.value)
+    if (!is_valid) e.target.classList.add('errorInput')
+    else e.target.classList.remove('errorInput')
+})              
 
+//cep
+document.querySelectorAll('input.mask').forEach($input => {
+    const field = $input.dataset.js
+    $input.addEventListener(
+        'input',
+        e => {
+            if (field != 'cep')
+        e.target.value = masks[field](e.target.value)
+        }, 
+    false
+    )
+})
+document.querySelector('#cep').addEventListener('input', e => {
+    let is_valid = valids.cep(e.target.value)
+    if (!is_valid) e.target.classList.add('errorInput')
+    else e.target.classList.remove('errorInput')
+})        
--- Atividades/A4/src/js/modules/valid.js
+++ Atividades/A4/src/js/modules/valid.js
@@ -1,74 +1,95 @@
-//valid.js
-
 export const valids = {
 
-      /*nome (value) {
-            
-        return value
-            if (nome (!contains(" ")))
-                  return true
-            else 
-                  alert('Insira pelo menos um sobrenome');
-            //return button = onClick=".errorInput"();
-      }, */
+   nome (value) {
+      //variável que guarda o tamanho do texto digitado em no campo nome 
+      let tamanho = value.length
+      
+      //retorna falso se: o nome tiver menos que 5 caracteres,
+      //estiver vazio ou não tiver sobrenome (espaço entre nomes)
+      if (value.length < 5 || value == '' || !value.trim().includes(' '))
+         return false
 
-      cpf (value) {
-            var CPF = value; // Recebe o valor digitado no campo
-            
-            // Verifica se o campo é nulo
-            if (CPF == "") {
-              alert('Este campo é de preenchimento obrigatório!');
-              return false;
-              }
-            
-            // Aqui começa a checagem do CPF
-            var POSICAO, I, SOMA, DV, DV_INFORMADO;
-            var DIGITO = new Array(10);
-            DV_INFORMADO = CPF.substr(9, 2); // Retira os dois últimos dígitos do número informado
-            
-            // Desemembra o número do CPF na array DIGITO
-            for (I=0; I<=8; I++) {
-              DIGITO[I] = CPF.substr( I, 1);
-            }
-            
-            // Calcula o valor do 10º dígito da verificação
-            POSICAO = 10;
-            SOMA = 0;
-               for (I=0; I<=8; I++) {
-                  SOMA = SOMA + DIGITO[I] * POSICAO;
-                  POSICAO = POSICAO - 1;
-               }
-            DIGITO[9] = SOMA % 11;
-               if (DIGITO[9] < 2) {
-                    DIGITO[9] = 0;
-            }
-               else{
-                   DIGITO[9] = 11 - DIGITO[9];
-            }
-            
-            // Calcula o valor do 11º dígito da verificação
-            POSICAO = 11;
-            SOMA = 0;
-               for (I=0; I<=9; I++) {
-                  SOMA = SOMA + DIGITO[I] * POSICAO;
-                  POSICAO = POSICAO - 1;
-               }
-            DIGITO[10] = SOMA % 11;
-               if (DIGITO[10] < 2) {
-                    DIGITO[10] = 0;
-               }
-               else {
-                    DIGITO[10] = 11 - DIGITO[10];
-               }
-            
-            // Verifica se os valores dos dígitos verificadores conferem
-            DV = DIGITO[9] * 10 + DIGITO[10];
-               if (DV != DV_INFORMADO) {
-                  alert('CPF inválido');
-                  value = "";
-                  value.focus();
-                  return false;
-               }
-      }
-  
+      // retorna verdadeiro pois passou das validações
+      return true
+   },       
+
+   //Corrigir a validação do CPF
+   cpf (value) {
+    function isCPF(cpf = 0) {	
+         value = cpf.replace(/\.|-/g,"");	
+
+         let soma = 0;
+         soma += cpf[0] * 10;
+         soma += cpf[1] * 9;
+         soma += cpf[2] * 8;
+         soma += cpf[3] * 7;
+         soma += cpf[4] * 6;
+         soma += cpf[5] * 5;
+         soma += cpf[6] * 4;
+         soma += cpf[7] * 3;
+         soma += cpf[8] * 2;
+         soma = (soma * 10) % 11;
+         if (soma == 10 || soma == 11) 
+         soma = 0;
+         if (soma != cpf[9]) 
+         return false;
+
+         soma = 0;
+         soma += cpf[0] * 11;
+         soma += cpf[1] * 10;
+         soma += cpf[2] * 9;
+         soma += cpf[3] * 8;
+         soma += cpf[4] * 7;
+         soma += cpf[5] * 6;
+         soma += cpf[6] * 5;
+         soma += cpf[7] * 4;
+         soma += cpf[8] * 3;
+         soma += cpf[9] * 2;
+         soma = (soma * 10) % 11;
+         if (soma == 10 || soma == 11) 
+            soma = 0;
+         if (soma != cpf[10]) 
+            return false;
+         //
+         return true;
+         isCPF(cpf);
+   }
+   },
+
+   dt_nasc (value) {
+      let tamanho = value.length
+      
+      if (value.length < 10)
+         return false
+
+      return true
+   },       
+
+   email (value) {
+      let tamanho = value.length
+      
+      if (value == '')
+         return false
+
+      return true
+   },   
+
+   fone (value) {
+      let tamanho = value.length
+      
+      if (value.length < 13)
+      
+         return false
+
+      return true
+   },   
+
+   cep (value) {
+      let tamanho = value.length
+      
+      if (value.length < 9)
+         return false
+
+      return true
+   },  
 }
