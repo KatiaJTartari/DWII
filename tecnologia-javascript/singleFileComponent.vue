d053a694a772d8a2e3f2a9481f7989cb272db028
KatiaJTartari <70042571+KatiaJTartari@users.noreply.github.com>
2021-11-07 12:44:59 -0300

    Add files via upload

---
+++ tecnologia-javascript/singleFileComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>{{ nome }}</h1> 
+        <input type="text" v-model="nome" />
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'katia-component',
+        data: () => ({ nome: 'KATIA' })
+    }
+</script>
+
+<style scoped>
+    h1 {
+        color: red;
+    }
+</style>
+ <template>
+    <div>
+        <h1>{{ nome }}</h1> 
+        <input type="text" v-model="nome" />
+    </div>
+</template>
+
+ 
