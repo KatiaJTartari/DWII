7a240b102a17ee9d5dcb14ccdcb2fd467bd75199
KatiaJTartari <70042571+KatiaJTartari@users.noreply.github.com>
2021-12-08 23:25:26 -0300

    Add files via upload

---
+++ projeto-spa/calculador/src/components/Calculator.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="page">
+    <ul>
+      <li v-for="produto in produtos" :key="produto.id">
+        <button class="menos" @click="produto.valor -= 10">-</button>
+        <button class="mais" @click="produto.valor += 10">+</button>
+        <h4><b>{{produto.nome}}</b></h4>
+        <hr>
+          Valor: R$ {{produto.valor}},00 
+          <span v-if="produto.valor === 100"><b> - PROMOÇÃO </b></span>
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+    data(){
+        return{
+            produtos: [
+              {
+                id: 1,
+                nome: "Ração",
+                valor: 5,
+              },
+              {
+                id: 2,
+                nome: "Veterinário",
+                valor: 200
+              },
+              {
+                id: 3,
+                nome: "Medicamentos",
+                valor: 50
+              },
+              {
+                id: 4,
+                nome: "Banho e tosa",
+                valor: 80
+              },
+              {
+                id: 5,
+                nome: "Controle de pulgas",
+                valor: 75
+              },
+              {
+                id: 6,
+                nome: "Vacina",
+                valor: 150,
+              },
+              {
+                id: 7,
+                nome: "Acessórios",
+                valor: 400
+              },
+              {
+                id: 8,
+                nome: "Outros",
+                valor: 90
+              }
+            ] 
+        }
+    },
+    computed: {
+      valorTotal () {
+        return this.produtos.reduce((sum, produto) => {
+          return sum + produto.valor
+        }, 0)
+      }
+    }
+}
+</script>
+
+<style scoped>
+  ul {
+    list-style-type: none;
+    font-family: helvetica;
+  }
+  li {
+    border-style: solid;
+    box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.3);
+    width: 20%;
+    text-align: center;
+    float: left;
+    margin-right: 10px;
+  }
+  .mais, .menos{
+    cursor: pointer;
+    font-weight: bold;
+    width: 30px;
+    height: 25px;
+    font-size: 15px;
+    font-family: fantasy;
+  }
+  
+</style>
---
+++ projeto-spa/calculador/src/components/Total.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h3>Total Gasto: R${{ message }},00</h3>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['message']
+  }
+</script>
+
+<style scoped>
+  h3 {
+    margin-top: 100px;
+    margin-left: 800px;
+    color: SeaGreen;
+    font-family: helvetica;
+  }
+</style>
