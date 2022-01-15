508bf56247c53b34024dcd886413c1d6f6b63449
KatiaJTartari <70042571+KatiaJTartari@users.noreply.github.com>
2022-01-14 22:53:43 -0300

    Add files via upload

---
+++ projeto-spa/calculador/src/components/BarraMenu.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <ul>
+      <li>Pet Calculador</li>
+      <li>Produtos</li>
+      <li>Contato</li>
+      <li>Adoção de Pet</li>
+    </ul>
+  </div>
+</template> 
+
+<style scoped>
+  ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    border: 1px solid #e7e7e7;
+    background-color: #f3f3f3;
+  }
+  li {
+    float: left;
+    display: block;
+    color: #666;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    font-family: helvetica;
+    font-weight: bold;
+    cursor: pointer;
+  }
+  li:hover {
+    color: white;
+    background-color: SeaGreen;
+  }
+</style>
